359e7f0c424c2153d5ce680cd7c9ff1a04f73352
kudrom <kudrom@riseup.net>
2014-02-19 00:48:25 +0100

    Testing filesystem

--- genesis2/utils/filesystem.py
+++ genesis2/utils/filesystem.py
@@ -1,26 +1,32 @@
 import os
-import logging
 
 
-def unlink_recursive(path):
-    """
-    from https://stackoverflow.com/questions/185936/delete-folder-contents-in-python
-    """
-    for file in os.listdir(path):
-        file_path = os.path.join(path, file)
-        try:
-            if os.path.isfile(file_path):
+def unlink_recursive(dir):
+    if os.path.isdir(dir):
+        for file in os.listdir(dir):
+            file_path = os.path.join(dir, file)
+            if os.path.isdir(file_path):
+                unlink_recursive(file_path)
+            else:
                 os.unlink(file_path)
-        except Exception, e:
-            logger = logging.getLogger("genesis2")
-            logger.error(e.message)
+        os.rmdir(dir)
+    else:
+        os.unlink(dir)
 
 
-def create_files(base_dir='', mode=0777, *files):
+def create_files(*files, **kwargs):
+    base_dir = os.getcwd() if 'base_dir' not in kwargs else kwargs['base_dir']
+    mode = 0777 if 'mode' not in kwargs else kwargs['mode']
     for file in files:
-        path = base_dir + file
+        # Allow calling create_files with base_dir and a file starting with /
+        if file[0] == '/' and 'base_dir' in kwargs:
+            file = file[1:]
+        # Interpret a calling without base_dir and a start different to /
+        if 'base_dir' not in kwargs and file[0] != '/' and not file.startswith('./'):
+            file = '/' + file
+        path = os.path.join(base_dir, file)
         file_dir = os.path.dirname(path)
         if not os.path.exists(file_dir):
-            os.makedirs(path, mode)
+            os.makedirs(file_dir, mode)
         if os.path.isdir(file_dir):
             open(path, "a").close()
--- genesis2/utils/tests/test_filesystem.py
+++ genesis2/utils/tests/test_filesystem.py
@@ -24,5 +24,22 @@ class TestFilesystem(TestCase):
         unlink_recursive(self.dir + '/dir1')
         self.assertFalse(os.path.exists(self.dir + '/dir1'))
 
+    def test_create_files_base_dir(self):
+        current_dir = os.path.dirname(__file__)
+        create_files('/dir1/file1', 'dir1/dir2/file12', base_dir=os.path.dirname(__file__))
+        self.assertIn('dir1', os.listdir(current_dir))
+        self.assertIn('file1', os.listdir(os.path.join(current_dir, 'dir1')))
+        self.assertIn('dir2', os.listdir(os.path.join(current_dir, 'dir1')))
+        self.assertIn('file12', os.listdir(os.path.join(current_dir, 'dir1/dir2')))
+
     def test_create_files(self):
-        pass
+        current_dir = os.path.dirname(__file__)
+        create_files(os.path.join(current_dir, 'dir1/file1'))
+        self.assertIn('dir1', os.listdir(current_dir))
+        self.assertIn('file1', os.listdir(os.path.join(current_dir, 'dir1')))
+
+    def test_create_files_without_leading_slash(self):
+        current_dir = os.path.dirname(__file__)
+        create_files('vagrant/genesis2/utils/tests/dir1/dir2/file12')
+        self.assertIn('dir2', os.listdir(os.path.join(current_dir, 'dir1')))
+        self.assertIn('file12', os.listdir(os.path.join(current_dir, 'dir1/dir2')))
